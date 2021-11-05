59b5b6aff1dadb10f0de4f51273e5b1e5ca67df0
mikewcm <90279864+mikewcm@users.noreply.github.com>
2021-11-05 17:10:59 -0400

    Update wordcombinebot.js
    basic functions of word bot

--- wordcombinebot.js
+++ wordcombinebot.js
@@ -9,73 +9,56 @@ var params = {q: "#georgiatech", count: 1, result_type: "recent"};
 //  Helper function to combine words
 //  Both words must be atleast 3 in length
 //  Searches the closest definition on Google, then combines the two.
+//  Free to change the functionality of these functions
 function combineWords(word1, word2) {
 	let combWord = "";
-	combWord = word1.substring(0, 3) + word2.substring();
+	combWord = word1.substring(0, 4) + word2.substring(word2.length - 3, word2.length);
+	combWord += ("\n" + combineDefinitions(word1, word2));
 	return combWord;
 }
-
-
-
-
-// This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
-function retweetLikeLatest() {
-	T.get('search/tweets', mediaArtsSearch, function (error, data) {
-	  // log out any errors and responses
-	  console.log(error, data);
-	  // If our search request to the server had no errors...
-	  if (!error) {
-	  	// ...then we grab the ID of the tweet we want to retweet...
-		var retweetLikeID = data.statuses[0].id_str;
-		// ...and then we tell Twitter we want to retweet it!
-		T.post('statuses/retweet/' + retweetLikeID, { }, function (error, response) {
-			if (response) {
-				console.log('Success! Check your bot, it should have retweeted something.')
-			}
-			// If there was an error with our Twitter call, we print it out here.
-			if (error) {
-				console.log('There was an error with Twitter:', error);
-			}
-		})
-		T.post('favorites/create/' + retweetLikeID, { }, function (error, response) {
-			if (response) {
-				console.log('Success! Just liked a post')
-			}
-			if (error) {
-				console.log('There was an error with Twitter:', error);
-			}
-		})
-	  }
-	  // However, if our original search request had an error, we want to print it out here.
-	  else {
-	  	console.log('There was an error with your hashtag search:', error);
-	  }
-	});
+function combineDefinitions(word1, word2) {
+	// Get google definition, splice them together.
+	// Idk how to get google definitions, so I would prefer someone else write this.
 }
 
-function postWord() {
-	T.get('search/tweets', mediaArtsSearch, function (error, data) {
-		console.log(error, data);
-		if (!error) {
-		  var postID = data.statuses[0].id_str;
-		  var tweet = { status: combineWords("", "") + "This word was created at this time, on this tweet, by " + data.statuses[0].author }
-		  T.post('statuses/update', tweet, function (error, response) {
-			  if (response) {
-				  console.log('Success! Check your bot, it should have posted something.')
-			  }
-			  if (error) {
-				  console.log('There was an error with Twitter:', error);
-			  }
-		  })
+function runBot() {
+	//  Gets the tweet, text, ID, and username
+	T.get('search/tweets', params, gotData);
+	function gotData(err, data, response) {
+		console.log(data.statuses[0].text);
+		var tweetText = data.statuses[0].text;
+		console.log(data.statuses[0].id_str);
+		var tweetID = data.statuses[0].id_str;
+		console.log(data.statuses[0].screen_name);
+		var tweetUser = data.statuses[0].screen_name;
+	}
+
+	//  Retweets and likes the post
+	T.post('statuses/retweet/', tweetID, { }, 
+		function(error, data, response) {
+			console.log('Successfully retweeted.');
 		}
-		else {
-			console.log('There was an error with your hashtag search:', error);
+	)
+	T.post('favorites/create/', {id: tweetID},
+		function(err, data, response) {
+			console.log('Successfully liked a post.');
 		}
-	});  
+	)
+
+	//  Creates the string that has combined the two words. T.post() function creates the post.
+	tweetToPost = { status: combineWords(tweetText, tweetText) + "\nThis word was created at this time, on this tweet, by user: " + tweetUser }
+	T.post('statuses/update', tweetToPost, tweeted);
+	function tweeted(err, data, response) {
+		if (err) {
+			console.log("Posting failed.");
+		} else {
+			console.log("Posting successful.");
+		}
+	}
 }
 
 // Try to retweet something as soon as we run the program...
-retweetLikeLatest();
+runBot();
 // ...and then every hour after that. Time here is in milliseconds, so
 // 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
 setInterval(retweetLatest, 1000 * 60 * 60);
