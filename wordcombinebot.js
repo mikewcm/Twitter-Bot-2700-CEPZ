a33eeed9c1ef7383a00362ccfd6b72eefdee244f
mikewcm <90279864+mikewcm@users.noreply.github.com>
2021-11-05 13:20:01 -0400

    Create wordcombinebot.js
    added updated twitterbot

---
+++ wordcombinebot.js
@@ -0,0 +1,54 @@
+// Our Twitter library
+var Twit = require('twit');
+
+// We need to include our configuration file
+var T = new Twit(require('./config.js'));
+
+// This is the URL of a search for the latest tweets on the '#mediaarts' hashtag.
+var mediaArtsSearch = {q: "#georgiatech", count: 1, result_type: "recent"}; 
+
+// This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
+function retweetLikeLatest() {
+	T.get('search/tweets', mediaArtsSearch, function (error, data) {
+	  // log out any errors and responses
+	  console.log(error, data);
+	  // If our search request to the server had no errors...
+	  if (!error) {
+	  	// ...then we grab the ID of the tweet we want to retweet...
+		var retweetLikeID = data.statuses[0].id_str;
+		// ...and then we tell Twitter we want to retweet it!
+		T.post('statuses/retweet/' + retweetLikeID, { }, function (error, response) {
+			if (response) {
+				console.log('Success! Check your bot, it should have retweeted something.')
+			}
+			// If there was an error with our Twitter call, we print it out here.
+			if (error) {
+				console.log('There was an error with Twitter:', error);
+			}
+		})
+		T.post('favorites/create/' + retweetLikeID, { }, function (error, response) {
+			if (response) {
+				console.log('Success! Just liked a post')
+			}
+			if (error) {
+				console.log('There was an error with Twitter:', error);
+			}
+		})
+	  }
+	  // However, if our original search request had an error, we want to print it out here.
+	  else {
+	  	console.log('There was an error with your hashtag search:', error);
+	  }
+	});
+}
+
+function combineWords(word1, word2) {
+	let combWord = "";
+	return combWord;
+}
+
+// Try to retweet something as soon as we run the program...
+retweetLikeLatest();
+// ...and then every hour after that. Time here is in milliseconds, so
+// 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
+setInterval(retweetLatest, 1000 * 60 * 60);
